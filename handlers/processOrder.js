70f87bc7547d9b0b6e9284a4d19d0b262099ab61
IRENE JOY TRINCHERA <irenejoy15@gmail.com>
2026-03-17 14:30:47 +0800

    SQS Process Order

---
+++ handlers/processOrder.js
@@ -0,0 +1,36 @@
+// STEP 6
+const {DynamoDBClient, PutItemCommand} = require('@aws-sdk/client-dynamodb');
+const dynamoDBClient = new DynamoDBClient({region: 'us-east-1'});
+
+exports.processOrder = async (event) => {
+    try{
+        for(const record of event.Records){
+            const orderData = JSON.parse(record.body);
+            const {id,productId,quantity,email,status,createdAt} = orderData;
+
+            // SEND A COMMAND TO DYNAMODB TO STORE THE ORDER IN THE ORDERS TABLE
+            await dynamoDBClient.send(new PutItemCommand({
+                TableName: process.env.DYNAMODB_TABLE,
+                Item: {
+                    id: { S: id },
+                    productId: { S: productId },
+                    quantity: { N: quantity.toString() },
+                    email: { S: email },
+                    status: { S: status },
+                    createdAt: { S: createdAt }
+                }
+            }));
+        }
+        return {
+            statusCode: 200,
+            body: JSON.stringify({msg: 'Orders processed successfully'}),
+        };
+    }catch(error){
+        console.error('Error processing order:', error);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({msg: 'Failed to process order'}),
+        };
+    }
+};
+// STEP 6
